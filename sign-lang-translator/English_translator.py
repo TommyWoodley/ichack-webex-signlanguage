024cfecd978cda31f3e3ac2aa997f3038df5fe0d
Minseo Lee <ml4320@ic.ac.uk>
2022-02-05 21:44:01 +0000

    Trained more data for eng trans

--- sign-lang-translator/English_translator.py
+++ sign-lang-translator/English_translator.py
@@ -7,7 +7,7 @@ max_num_hands = 1
 gesture = {
     0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 5:'F', 6:"G", 7:'H', 8:'I', 9:'J', 10: 'K',
     11:'L', 12:'M', 13:'N', 14:'O', 15:'P', 16:'Q', 17:'R', 18:'S', 19:'T', 20:'U',
-    21:'V', 22:'W', 23:'X', 24:'Y', 25:'Z',
+    21:'V', 22:'W', 23:'X', 24:'Y', 25:'Z', 26:' ', 27:'.', 28:'',
 }
 
 # MediaPipe hands model
@@ -19,7 +19,7 @@ hands = mp_hands.Hands(
     min_tracking_confidence=0.5)
 
 # Gesture recognition model
-collect_data_file = open('data/english_data.txt', 'a')
+collect_data_file = open('data/collectedData.txt', 'a')
 
 file = np.genfromtxt('data/english_gesture_train.csv', delimiter=',')
 angle = file[:,:-1].astype(np.float32)
@@ -97,6 +97,7 @@ while cap.isOpened():
 
     if keyboard.is_pressed('e'):
         collect_data_file.close()
+
         break
 
 
--- sign-lang-translator/translator.py
+++ sign-lang-translator/translator.py
@@ -6,10 +6,11 @@ import numpy as np
 
 max_num_hands = 3
 english_gesture = {
-    0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 5:'F', 6:"G",
-    7:'H', 8:'I', 9:'K', 10:'L', 11:'M', 12:'N', 13:'O',
-    14:'Q',
+    0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 5:'F', 6:"G", 7:'H', 8:'I', 9:'J', 10: 'K',
+    11:'L', 12:'M', 13:'N', 14:'O', 15:'P', 16:'Q', 17:'R', 18:'S', 19:'T', 20:'U',
+    21:'V', 22:'W', 23:'X', 24:'Y', 25:'Z', 26:' ', 27:'.', 28:'',
 }
+
 chinese_gesture = {
     0:'i', 1:'c', 2:'h', 3:'a', 4:'k', 5:'ch', 6:'ng'
 }
@@ -88,13 +89,11 @@ while cap.isOpened():
             if keyboard.is_pressed('s'):
                 for num in angle:
                     num = round(num, 6)
-                    print (num)
                     collect_data_file.write(str(num))
-                    # print("Read line: " + collect_data_file.readline())
                     collect_data_file.flush()
                     collect_data_file.write(",")
                     collect_data_file.flush()
-                collect_data_file.write("4.000000")
+                collect_data_file.write("6.000000")
                 collect_data_file.flush()
                 collect_data_file.write("\n")
                 collect_data_file.flush()
@@ -105,32 +104,8 @@ while cap.isOpened():
             data = np.array([angle], dtype=np.float32)
             ret, results, neighbours, dist = knn.findNearest(data, 3)
             idx = int(results[0][0])
-            if delay_time_passed:
-                t_end = time.time() + 1
-                start_idx = idx
-
-            # Draw gesture result
-            # if idx in rps_gesture.keys():
-                #cv2.putText(img, text=rps_gesture[idx].upper(), org=(int(res.landmark[0].x * img.shape[1]), int(res.landmark[0].y * img.shape[0] + 20)), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
 
             # Other gestures
-
-            if idx in english_gesture.keys():
-                if time.time() < t_end:
-                    delay_time_passed = False
-                    print ("The time now is: " + str(time.time()))
-                    print ("Time to end is" + str(t_end))
-                    # unchanged = idx != prev_idx
-                    # prev_idx = idx
-                else:
-                    delay_time_passed = True
-                    print("delay time is passed")
-                    if start_idx == idx:
-                        if lang == "2":
-                            output_text += chinese_gesture[idx].upper()
-                        else:
-                            output_text += english_gesture[idx].upper()
-
             if lang == "2":
                 cv2.putText(img, text=chinese_gesture[idx].upper(), org=(int(res.landmark[0].x * img.shape[1]),
                             int(res.landmark[0].y * img.shape[0] + 20)), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
@@ -146,7 +121,7 @@ while cap.isOpened():
     if cv2.waitKey(1) == ord('q'):
         break
 
-    if keyboard.is_pressed('e'):
+    if keyboard.is_pressed('esc'):
         collect_data_file.close()
         print("The output text is: " + output_text)
         break
