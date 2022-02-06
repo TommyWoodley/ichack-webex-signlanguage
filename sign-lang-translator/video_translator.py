96ede3d61dcd56061e639b2e535a1cdc53260cb0
Minseo Lee <ml4320@ic.ac.uk>
2022-02-06 09:13:15 +0000

    Video translator has text to speech and subtitles

--- sign-lang-translator/video_translator.py
+++ sign-lang-translator/video_translator.py
@@ -3,12 +3,31 @@ import time
 import keyboard as keyboard
 import mediapipe as mp
 import numpy as np
+import gtts
+import pyttsx3 as pyttsx3
+from playsound import playsound
+
+def draw_text(img, text,
+          font=cv2.FONT_HERSHEY_PLAIN,
+          pos=(0, 0),
+          font_scale=3,
+          font_thickness=2,
+          text_color=(0, 255, 0),
+          text_color_bg=(0, 0, 0)
+          ):
+
+    x, y = pos
+    text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
+    text_w, text_h = text_size
+    cv2.rectangle(img, pos, (x + text_w, y + text_h), text_color_bg, -1)
+    cv2.putText(img, text, (x, y + text_h + font_scale - 1), font, font_scale, text_color, font_thickness)
+
 
 max_num_hands = 2
 english_gesture = {
     0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 5:'F', 6:"G", 7:'H', 8:'I', 9:'J', 10: 'K',
     11:'L', 12:'M', 13:'N', 14:'O', 15:'P', 16:'Q', 17:'R', 18:'S', 19:'T', 20:'U',
-    21:'V', 22:'W', 23:'X', 24:'Y', 25:'Z', 26:' ', 27:'.', 28:'',
+    21:'V', 22:'W', 23:'X', 24:'Y', 25:'Z', 26:' ', 27:'.', 28:'spiderman!',
 }
 
 chinese_gesture = {
@@ -16,29 +35,9 @@ chinese_gesture = {
 }
 
 sentence = []
-
-
-def store_char(ch):
-    if ch == '.':
-        sentence.append(ch)
-        sub = ''.join(sentence)
-        sentence.clear()
-        print (sub)
-        return sub
-
-    if len(sentence):
-        if ch != sentence[-1]:
-            sentence.append(ch)
-    else:
-        sentence.append(ch)
-
-    return ''
-
-
 output_text = ""
 delay_time_passed = True
 t_end = time.time() + 2
-subtitle = ""
 
 # MediaPipe hands model
 mp_hands = mp.solutions.hands
@@ -65,7 +64,6 @@ else:
 
 # Gesture recognition model
 collect_data_file = open('data/collectedData.txt', 'a')
-
 
 angle = file[:,:-1].astype(np.float32)
 label = file[:, -1].astype(np.float32)
@@ -122,7 +120,6 @@ while cap.isOpened():
                 collect_data_file.close()
 
             # Inference gesture
-            # output_text = ""
             data = np.array([angle], dtype=np.float32)
             ret, results, neighbours, dist = knn.findNearest(data, 3)
             idx = int(results[0][0])
@@ -132,17 +129,9 @@ while cap.isOpened():
                 start_idx = idx
 
             # Draw gesture result
-            # if idx in rps_gesture.keys():
-                #cv2.putText(img, text=rps_gesture[idx].upper(), org=(int(res.landmark[0].x * img.shape[1]), int(res.landmark[0].y * img.shape[0] + 20)), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
-
-            # Other gestures
             if idx in english_gesture.keys():
                 if time.time() < t_end:
                     delay_time_passed = False
-                    # print ("The time now is: " + str(time.time()))
-                    # print ("Time to end is" + str(t_end))
-                    # unchanged = idx != prev_idx
-                    # prev_idx = idx
                 else:
                     delay_time_passed = True
                     print("delay time is passed")
@@ -153,47 +142,45 @@ while cap.isOpened():
                             output_text += chinese_gesture[idx].upper()
 
                         print(output_text)
-                        # subtitle += english_gesture[idx].upper()
 
+                    # Deleting same characters in a row (should not be a problem if right timing used)
                     if len(output_text) > 1:
                         if output_text[-1] == output_text[-2]:
-                            output_text = output_text[:-2]
-
-            # if lang == "2":
-            #     cv2.putText(img, text=chinese_gesture[idx].upper(), org=(int(res.landmark[0].x * img.shape[1]),
-            #                 int(res.landmark[0].y * img.shape[0] + 20)), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-            #                 fontScale=2.5, color=(0, 0, 255), thickness=2)
-            # else:
-            #     cv2.putText(img, text=english_gesture[idx].upper(),
-            #                 org=(int(res.landmark[0].x * img.shape[1]), int(res.landmark[0].y * img.shape[0] + 20)),
-            #                 fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=2.5, color=(255, 255, 255), thickness=2)
-            #
-            # mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
+                            output_text = output_text[:-1]
 
             if lang == "2":
                 cv2.putText(img, text=chinese_gesture[idx].upper(), org=(int(res.landmark[0].x * img.shape[1]),
                             int(res.landmark[0].y * img.shape[0] + 20)), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
                             fontScale=3.5, color=(0, 0, 255), thickness=2)
-                sub = store_char(chinese_gesture[idx])
             else:
                 cv2.putText(img, text=english_gesture[idx].upper(),
                             org=(int(res.landmark[0].x * img.shape[1]), int(res.landmark[0].y * img.shape[0] + 20)),
                             fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=3.5, color=(255, 255, 255), thickness=2)
-                sub = store_char(english_gesture[idx])
 
             mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
 
-            if sub != '':
-                cv2.putText(img, text=sub,
-                            org=(300,300),
-                            fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=2, color=(255, 255, 255), thickness=2)
-
-            # sub = ''
-
+            if output_text != '':
+                # cv2.putText(img, text=output_text,
+                #             org=(250,450),
+                #             fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 0, 0), thickness=2)
+                draw_text(img, output_text,
+                          font=cv2.FONT_HERSHEY_PLAIN,
+                          pos=(250,400),
+                          font_scale=4,
+                          font_thickness=2,
+                          text_color=(255, 255, 255),
+                          text_color_bg=(0, 0, 0)
+                          )
 
     cv2.imshow('Sign language translator', img)
     if cv2.waitKey(1) == ord('q'):
         break
+
+    if keyboard.is_pressed('p'):
+        engine = pyttsx3.init()
+        engine.setProperty("rate", 100)
+        engine.say(output_text)
+        engine.runAndWait()
 
     if keyboard.is_pressed('esc'):
         collect_data_file.close()
